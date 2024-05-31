c388a6fb7ee831096f1f24e2c5bd1e220a5426d7
manish@sethis.in <manish@sethis.in>
2024-05-31 07:42:12 +0000

    Fix the webhook

--- terraform/ACM-Lambda/code/main.py
+++ terraform/ACM-Lambda/code/main.py
@@ -5,14 +5,9 @@ import os
 import urllib3
 
 http = urllib3.PoolManager()
-secretsmanager = boto3.client('secretsmanager')
-acm = boto3.client('acm')
 
-def get_secret(secret_name):
-    response = secretsmanager.get_secret_value(SecretId=secret_name)
-    return json.loads(response['SecretString'])[secret_name]
-
-def get_certificate_details(cert_arn):
+def get_certificate_details(cert_arn, region):
+    acm = boto3.client('acm', region_name=region)
     response = acm.describe_certificate(CertificateArn=cert_arn)
     return response['Certificate']
 
@@ -25,29 +20,33 @@ def send_slack_message(webhook_url, message):
     return response
 
 def lambda_handler(event, context):
-    slack_webhook_url = get_secret(os.environ['SLACK_WEBHOOK_URL'])
-    region = os.environ['REGION']
+    slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
     alert_before_days = 30
-    certificates = acm.list_certificates(CertificateStatuses=['ISSUED'])
-    
-    for cert in certificates['CertificateSummaryList']:
-        cert_details = get_certificate_details(cert['CertificateArn'])
-        not_after = cert_details['NotAfter']
-        days_to_expiry = (not_after - datetime.utcnow()).days
-        
-        if days_to_expiry <= alert_before_days:
-            arn = cert_details['CertificateArn']
-            san = cert_details['SubjectAlternativeNames']
-            exp_date = not_after.strftime('%Y-%m-%d')
-            message = (
-                f"SSL Certificate is expiring soon!\n"
-                f"ARN: {arn}\n"
-                f"Region: {region}\n"
-                f"Subject Alternative Names: {', '.join(san)}\n"
-                f"Expiry Date: {exp_date}\n"
-                f"Days to Expiry: {days_to_expiry} days"
-            )
-            send_slack_message(slack_webhook_url, message)
+
+    ec2 = boto3.client('ec2')
+    regions = [region['RegionName'] for region in ec2.describe_regions()['Regions']]
+
+    for region in regions:
+        acm = boto3.client('acm', region_name=region)
+        certificates = acm.list_certificates(CertificateStatuses=['ISSUED'])
+        for cert in certificates['CertificateSummaryList']:
+            cert_details = get_certificate_details(cert['CertificateArn'], region)
+            not_after = cert_details['NotAfter']
+            days_to_expiry = (not_after - datetime.utcnow()).days
+            
+            if days_to_expiry <= alert_before_days:
+                arn = cert_details['CertificateArn']
+                san = cert_details['SubjectAlternativeNames']
+                exp_date = not_after.strftime('%Y-%m-%d')
+                message = (
+                    f"SSL Certificate is expiring soon!\n"
+                    f"ARN: {arn}\n"
+                    f"Region: {region}\n"
+                    f"Subject Alternative Names: {', '.join(san)}\n"
+                    f"Expiry Date: {exp_date}\n"
+                    f"Days to Expiry: {days_to_expiry} days"
+                )
+                send_slack_message(slack_webhook_url, message)
     
     return {
         'statusCode': 200,
